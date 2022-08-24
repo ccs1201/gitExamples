88ae0ffbb53fa0427cc7f05f7f6a4ca5be4f65f5
Cleber Souza <ccs1201@gmail.com>
2022-08-24 17:42:18 -0300

    #dados
    Commit #14
    Adicionado App.bibliotecario
    refatorado bibliotecario.java para Bibliotecario.java
    
    Signed-off-by: Cleber Souza <ccs1201@gmail.com>

--- src/main/java/com/ccs/gitexamples/biblioteca/App.java
+++ src/main/java/com/ccs/gitexamples/biblioteca/App.java
@@ -4,4 +4,5 @@ public class App {
     Livro livro = new Livro();
     Usuario usuario = new Usuario();
     Emprestimo emprestimo = new Emprestimo();
+    Bibliotecario bibliotecario = new Bibliotecario();
 }
--- src/main/java/com/ccs/gitexamples/biblioteca/bibliotecario.java
+++
@@ -1,7 +0,0 @@
-package com.ccs.gitexamples.biblioteca;
-
-public class bibliotecario {
-
-    private int id;
-    private String nome;
-}
